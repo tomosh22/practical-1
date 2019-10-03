d806ae309f7939139ae7b4fd08190fd7fef98721
b9014614 <t.o'shaughnessy1@newcastle.ac.uk>
2019-10-03 10:48:15 +0100

    added camera controls and help menu

--- hello_world.py
+++ hello_world.py
@@ -3,6 +3,8 @@ from math import pi, sin, cos
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
+from direct.gui.DirectGui import *
+import keyboard
 
 
 class MyApp(ShowBase):
@@ -20,7 +22,10 @@ class MyApp(ShowBase):
         # Add the spinCameraTask procedure to the task manager.
 
         self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
-
+        self.angleDegrees = 0
+        self.angleRadians = 0
+        self.cameraZOffset = 0
+        self.cameraXOffset = 0
         # Load and transform the panda actor.
         if drawPanda:
             self.pandaActor = Actor("models/panda-model",
@@ -29,17 +34,33 @@ class MyApp(ShowBase):
             self.pandaActor.reparentTo(self.render)
             # Loop its animation.
             self.pandaActor.loop("walk")
+        if helpMenu:
+            OnscreenText(text = "Help Menu", pos = (0.9,0.8),scale = 0.1,fg = (255,255,255,255))
+            OnscreenText(text="Move Camera Up: W", pos=(0.9, 0.7), scale=0.1,fg = (255,255,255,255))
+            OnscreenText(text="Move Camera Left: A", pos=(0.9, 0.6), scale=0.1,fg = (255,255,255,255))
+            OnscreenText(text="Move Camera Down: S", pos=(0.9, 0.5), scale=0.1,fg = (255,255,255,255))
+            OnscreenText(text="Move Camera Right: D", pos=(0.9, 4), scale=0.1,fg = (255,255,255,255))
+            OnscreenText(text="Only works with spin camera off", pos=(0.9, 0.45), scale=0.05, fg=(255, 255, 255, 255))
+
 
     # Define a procedure to move the camera.
     def spinCameraTask(self, task):
         if spinCamera:
-            angleDegrees = task.time * 6.0
-            angleRadians = angleDegrees * (pi / 180.0)
+            self.angleDegrees = task.time * 6.0
+            self.angleRadians = self.angleDegrees * (pi / 180.0)
         else:
-            angleDegrees = 0
-            angleRadians = 0
-        self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
-        self.camera.setHpr(angleDegrees, 0, 0)
+            if keyboard.is_pressed("w"):
+                self.cameraZOffset += 0.05
+            if keyboard.is_pressed("s"):
+                self.cameraZOffset -= 0.05
+            if keyboard.is_pressed("a"):
+                self.cameraXOffset -= 0.05
+            if keyboard.is_pressed("d"):
+                self.cameraXOffset += 0.05
+
+
+        self.camera.setPos(20 * sin(self.angleRadians) + self.cameraXOffset, -20.0 * cos(self.angleRadians), 3 + self.cameraZOffset)
+        self.camera.setHpr(self.angleDegrees, 0, 0)
         return Task.cont
 spinCamera = True
 drawPanda = True
@@ -53,9 +74,6 @@ if input() != "yes":
 print("Help Menu?")
 if input() != "yes":
     helpMenu = False
-if helpMenu:
-    print("Help Menu")
 
 app = MyApp()
 app.run()
-
